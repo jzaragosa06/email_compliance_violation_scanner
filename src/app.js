81f3ee5b3d523ac82f25a4f229aa22b9dacd31dc
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-05-19 12:37:40 +0800

    Add database configuration and initialize Sequelize connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 
 const express = require('express')
 const app = express();
+const sequelize = require("./config/db");
+const { allRoutes } = require('./routes');
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
 
+//routes
+allRoutes(app);
+
+sequelize.sync({
+    force: false,
+}).then(() => {
+    console.log('Database synced');
+    app.listen(3000, () => {
+        console.log('Server running on http://localhost:3000');
+    });
+})
+
 app.get('/', (req, res) => {
     res.status(200).json({message: "okay"})
 });
+
+
 
 module.exports = app; 
