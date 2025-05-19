13c90545c0d0ef15a686d57d0671e0608f907a08
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-05-19 20:24:14 +0800

    refactor: remove the start of the app from the sequelize callback

--- src/app.js
+++ src/app.js
@@ -10,14 +10,12 @@ app.use(express.json());
 //routes
 app.use('/api', routes); 
 
+
 sequelize.sync({
-    force: true,
-}).then(() => {
-    console.log('Database synced');
-    app.listen(3000, () => {
-        console.log('Server running on http://localhost:3000');
-    });
-})
+    force: false,
+});
+
+
 
 app.get('/', (req, res) => {
     res.status(200).json({message: "okay"})
