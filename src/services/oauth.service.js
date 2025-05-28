80b3dd17248f80e7a62c5ac3bca408005a80f63b
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-05-28 14:55:00 +0800

    feat: fetch access token using user's refresh token

--- src/services/oauth.service.js
+++ src/services/oauth.service.js
@@ -8,13 +8,30 @@ exports.generateAuthURL = async (org_user_account_id, state) => {
     const authUrl = oauth2Client.generateAuthUrl(
         {
             access_type: "offline",
-            scope: ['https://www.googleapis.com/auth/gmail.readonly'],
+            scope: [
+                'https://www.googleapis.com/auth/gmail.readonly',
+                // 'https://www.googleapis.com/auth/gmail.settings.basic',  
+                // 'https://www.googleapis.com/auth/gmail.settings.sharing'
+            ],
             state: `${org_user_account_id}:${state}`,
             prompt: 'consent',
         }
     );
 
     return authUrl;
+}
 
+exports.getAccessToken = async (refresh_token) => {
+    const oauth2Client = await createOAuth2Client();
 
+    //set the refresh token to target specific user account
+    oauth2Client.setCredentials({
+        refresh_token: refresh_token,
+    });
+
+    const { token } = await oauth2Client.getAccessToken();
+
+    if (!token) throw new Error("Error getting an access token");
+
+    return token;
 }
