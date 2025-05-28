47193f5f56ca3f7c952db5b188d854f29c16b1fe
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-05-28 14:54:00 +0800

    feat: enhance org user account service with new methods for email retrieval and filtering

--- src/services/org_user_account.service.js
+++ src/services/org_user_account.service.js
@@ -24,6 +24,38 @@ exports.findOneOrgUserAccountsById = async (org_user_account_id) => {
     });
 }
 
+exports.findOneOrgUserAccountsByEmailAndOrgId = async (org_id, email) => {
+    return await OrgUserAccount.findOne({
+        where: { org_id: org_id, email: email },
+        include: [
+            {
+                model: EmailAccountAuth,
+            },
+            {
+                model: EmailAccountStatus,
+            },
+            {
+                model: EmailAnalysisLog
+            }
+        ]
+    });
+}
+
+exports.findAllAuthenticatedOrgUserAccount = async (org_id) => {
+    const org_user_accounts = await OrgUserAccount.findAll({
+        where: { org_id: org_id },
+        include: [
+            {
+                model: EmailAccountStatus,
+                where: { is_authenticated: true },
+                required: true
+            }
+        ]
+    });
+
+    return org_user_accounts;
+}
+
 exports.addOrgUserAccounts = async (org_id, user_id, emails) => {
     const org = await findOrgByOrgId(org_id);
     const user = await findUserByUserId(user_id);
@@ -109,4 +141,24 @@ exports.deleteOneOrgUserAccounById = async (org_user_account_id) => {
     if (!org_user_account) throw new Error("Organization user account not found");
 
     return await org_user_account.destroy();
+}
+
+exports.cleanForNewOrgUserAccounts = async (org_id, emails) => {
+    //this is an array of object: [{email}, {email}]
+    const org_user_accounts = await OrgUserAccount.findAll({
+        where: {
+            org_id: org_id
+        },
+        attributes: ['email'],
+        raw: true,
+    });
+
+    //we can convert this into an array and use .include() to check if email exist O(n)
+    //or we can convert this into set and use the .has method for faster look up O(1)
+    const existingEmails = new Set(org_user_accounts.map(acc => acc.email));
+
+    //then filter for emails not in set
+    const newEmails = emails.filter(email => !existingEmails.has(email));
+    return newEmails;
+
 }
